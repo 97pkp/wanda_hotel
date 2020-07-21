a304372a569dc0ac9beb94518e6b4982952896f3
kyz <1104152611@qq.com>
2020-07-21 18:46:51 +0800

    集团首页

---
+++ component/bracket/index.js
@@ -0,0 +1,23 @@
+// component/bracket/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    txt: '超级屯游节测试'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
