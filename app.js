6848c87fb825ee6c2f202d94c02d26b15e890bc7
kyz <1104152611@qq.com>
2020-07-24 18:33:17 +0800

    update

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ App({
         }
       })
     })
+    // 索引列表
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let capsule = wx.getMenuButtonBoundingClientRect();
+        if (capsule) {
+          this.globalData.Custom = capsule;
+          this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
+        } else {
+          this.globalData.CustomBar = e.statusBarHeight + 50;
+        }
+      }
+    })
 
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
