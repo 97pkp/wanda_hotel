2aaf29f5b4010508018ef0a56066052c57bdf53e
dengzhen <zhendenga@isoftstone.com>
2020-07-29 14:10:47 +0800

    订单列表礼券部分修改

--- pages/orderList/components/Coupon/index.js
+++ pages/orderList/components/Coupon/index.js
@@ -11,13 +11,54 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    couponList: [{
+      name: '库尔勒万达锦华酒店',
+      src: '/images/shopcar.jpg',
+      info: '豪华河景单卧别墅2晚+双早+主题乐园/雪世界双人门票',
+      collectInfo: '5.5-5.7满一百减10',
+      moneny: 80,
+      count: 1,
+      id: 101,
+      num: '2561 8956',
+      person: '小妹',
+      couponType: 1, // 1 已转赠 0 已使用
+      couponTypeStr: '正在等待'
+    }, {
+        name: '库尔勒万达锦华酒店',
+        src: '/images/shopcar.jpg',
+        info: '豪华河景单卧别墅2晚+双早+主题乐园/雪世界双人门票',
+        collectInfo: '5.5-5.7满一百减10',
+        moneny: 80,
+        count: 1,
+        id: 101,
+        num: '2561 8956',
+        person: '小妹',
+        couponType: 1, // 1 已转赠 0 已使用
+        couponTypeStr: ''
+      }, {
+        name: '北京万达锦华酒店',
+        src: '/images/shopcar.jpg',
+        info: '豪华河景单卧别墅2晚+双早+主题乐园/雪世界双人门票',
+        collectInfo: '5.5-5.7满一百减10',
+        moneny: 180,
+        count: 1,
+        id: 102,
+        num: '2561 8957',
+        person: '小妹',
+        couponType: 0, // 1 已转赠 0 已使用
+        couponTypeStr: '正在等待'
+      }],
+      changeValue: 'all'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    couponChange(e){
+      this.setData({
+        changeValue: e.currentTarget.dataset.value
+      })
+    }
   }
 })
